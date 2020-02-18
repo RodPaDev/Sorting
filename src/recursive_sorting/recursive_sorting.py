67ba236d7df469aa6804e97a8492310a6626d766
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-02-18 18:38:14 +0100

    mvp complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,43 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
+import math
+
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+    mergedArr = []
+
+    while arrA != [] and arrB != []:
+        if arrA[0] > arrB[0]:
+            mergedArr.append(arrB[0])
+            arrB.pop(0)
+        else:
+            mergedArr.append(arrA[0])
+            arrA.pop(0)
+
+    while arrA != []:
+        mergedArr.append(arrA[0])
+        arrA.pop(0)
+    while arrB != []:
+        mergedArr.append(arrB[0])
+        arrB.pop(0)
+
+    return mergedArr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if len(arr) <= 1 :
+        return arr
 
-    return arr
+    # TO-DO
+    median = math.floor(len(arr) / 2)
+    arrA = arr[0:median]
+    arrB = arr[median:]
+    
+    arrA = merge_sort(arrA)
+    arrB = merge_sort(arrB)
+
+    return merge(arrA, arrB)
+
+print(merge_sort([9,8,7,6,5,46,3,2,1]))
 
 
 # STRETCH: implement an in-place merge sort algorithm
